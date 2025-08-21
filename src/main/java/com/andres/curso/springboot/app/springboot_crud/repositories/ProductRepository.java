ed74bae821d46c280071b6cd72601c3cb718c95e
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-08-21 16:36:48 -0400

    Creando clases entity y repository

---
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.andres.curso.springboot.app.springboot_crud.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.andres.curso.springboot.app.springboot_crud.entities.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+}

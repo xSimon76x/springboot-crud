04803635b7a1ce16a45583841e91ecaec27c3ff0
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-08-21 15:58:51 -0400

    files project create

---
+++ src/test/java/com/andres/curso/springboot/app/springboot_crud/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.curso.springboot.app.springboot_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

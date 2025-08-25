58e67d8fd25e02576c3eb873b7825bfa7a197896
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-08-25 16:57:37 -0400

    Implementando Login successful & Generando el firmando el token JWT

--- src/main/java/com/andres/curso/springboot/app/springboot_crud/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/security/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,10 @@
 package com.andres.curso.springboot.app.springboot_crud.security.filter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,12 +17,20 @@ import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter { //? Filtro de autenticacion basado en JWT
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter { 
+    //? Clase Filtro para autenticar y generar el token JWT
+    //? Se ejecuta cuando se hace una peticion a /login con el metodo POST
 
     private AuthenticationManager authenticationManager;
+
+    //? Clave secreta para firmar el token
+    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
@@ -50,6 +62,33 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     }
 
+    @Override
+    protected void successfulAuthentication(
+        HttpServletRequest request, 
+        HttpServletResponse response, 
+        FilterChain chain,
+        Authentication authResult
+    ) throws IOException, ServletException {
+        User user = (User) authResult.getPrincipal();
+        String username = user.getUsername();
+        Map<String, String> body = new HashMap<>();
+        
+        //? Generamos el token, firmandolo con la clave secreta, y poniendole el username como subject
+        String token = Jwts.builder().subject(username).signWith(SECRET_KEY).compact();
+
+        //? Lo agregamos en la respuesta
+        response.addHeader("Authorization", "Bearer " + token);
+
+        body.put(token, token);
+        body.put("username", username);
+        body.put("message", String.format("Hola %s, has iniciado sesion con exito!", username));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType("application/json");
+        response.setStatus(200);
+    }
+
+    
     
 
 }

54f87190f3c4c5556f6a248ba44f63406553c303
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-08-25 09:59:36 -0400

    Asignando roles y encriptando password con Bcrypt

--- src/main/java/com/andres/curso/springboot/app/springboot_crud/services/UserServiceImpl.java
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/services/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.andres.curso.springboot.app.springboot_crud.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.andres.curso.springboot.app.springboot_crud.entities.Role;
 import com.andres.curso.springboot.app.springboot_crud.entities.User;
 import com.andres.curso.springboot.app.springboot_crud.repositories.RoleRepository;
 import com.andres.curso.springboot.app.springboot_crud.repositories.UserRepository;
@@ -32,6 +35,19 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public User save(User user) {
+
+        Optional<Role>  optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> roles = new ArrayList<>();
+
+        optionalRoleUser.ifPresent(roles::add);
+
+        if( user.isAdmin()){
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(roles::add);
+        }
+        user.setRols(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return userRepository.save(user);
     }
         

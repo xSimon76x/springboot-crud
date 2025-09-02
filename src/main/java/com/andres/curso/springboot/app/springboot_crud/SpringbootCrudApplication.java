d2835321f756db5aa1884c9299919eeb383be255
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-09-02 13:09:16 -0400

    configuracion para tomcat

--- src/main/java/com/andres/curso/springboot/app/springboot_crud/SpringbootCrudApplication.java
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/SpringbootCrudApplication.java
@@ -2,9 +2,12 @@ package com.andres.curso.springboot.app.springboot_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootCrudApplication {
+public class SpringbootCrudApplication 
+	extends SpringBootServletInitializer //? Para que funcione el .war en un servidor externo como Tomcat v.10
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCrudApplication.class, args);
